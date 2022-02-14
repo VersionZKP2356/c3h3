d219a0310f085d65c06dabe3d2e4c0671bc26f26
VersionZKP2356 <62052278+VersionZKP2356@users.noreply.github.com>
2022-02-14 19:03:33 +0800

    Add files via upload

---
+++ assets/js/qq.js
@@ -0,0 +1,9 @@
+var qq_chat = true;
+
+    function PlayJsAdPopWin() {
+    if (qq_chat) {
+    popwin = window.location.href = 'tencent://message/?uin=3298099069&Site=C3H3&Menu=yes'
+    
+    }
+    
+    };
